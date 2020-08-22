29672ed6e5ba1af6468bcfd28c8bc7432c63aef8
Rayan IG <raymoun17@gmail.com>
2020-08-21 20:49:48 -0400

    imlemented a dynamic background using the pexels api and a redesigned hotkey system

--- js/hotkeys.js
+++ js/hotkeys.js
@@ -1,17 +1,91 @@
 //A dictionary containing the sites with the corresponding hotkeys
-const sites = { "R": "https://www.reddit.com", "T": "http://www.4channel.org/g/", "D": "http://www.discord.com/login" }
+const sites = {};
 
+const categories = {
+	social: {
+		reddit: {
+			R: "https://www.reddit.com",
+		},
+		discord: {
+			D: "https://www.discord.com/login",
+		},
+		"4chan": {
+			4: "https://www.4chan.org/",
+		},
+	},
+	learn: {
+		youtube: {
+			Y: "https://www.youtube.com",
+		},
+		freecodecamp: {
+			F: "https://www.freecodecamp.org",
+		},
+		w3school: {
+			W: "https://www.w3schools.com/",
+		},
+	},
+	work: {
+		github: {
+			G: "https://github.com",
+		},
+		"codepen.io": {
+			C: "https://codepen.io/pen",
+		},
+		coolors: {
+			O: "https://coolors.co",
+		},
+		pixlr: {
+			X: "https://pixlr.com/x/",
+		},
+	},
+	misc: {
+		"yewtu.be": {
+			Y: "https://yewtu.be",
+		},
+		mega: {
+			M: "https://mega.nz",
+		},
+		TPB: {
+			T: "https://thepiratebay.org",
+		},
+		"1337x": {
+			1: "https://1337x.to",
+		},
+	},
+};
+
+// loop thro the categories and dynamically fill the sites object
+function getSites() {
+	for (category in categories) {
+		let category_card = document.getElementById(category);
+		console.log(category_card);
+		const category_sites = {};
+		for (site in categories[category]) {
+			for (hotkey in categories[category][site]) {
+				let link = categories[category][site][hotkey];
+				sites[hotkey] = link;
+				category_sites[site] = link;
+			}
+		}
+		for (category_site in category_sites) {
+			const link = category_sites[category_site];
+			category_card.innerHTML += `<li><a href=${link}>${category_site}</a></li>`;
+		}
+	}
+}
 
 // social stuff
-window.addEventListener('keydown', (event) => {
-    //  Get the ASCII key code (int) of the key pressed then convert it to (string) the corresponding letter name
-    let pressed_key = String.fromCharCode(event.keyCode);
-    event.preventDefault();
-    let keys = Object.keys(sites);
-    if (keys.includes(pressed_key)) {
-        //  Lookup the corresponding key value in the dictionary
-        let target_site = sites[pressed_key];
-        //  Change windows location to the corresponding site
-        window.location.href = target_site;
-    }
-})
+window.addEventListener("keydown", (event) => {
+	//  Get the ASCII key code (int) of the key pressed then convert it to (string) the corresponding letter name
+	let pressed_key = String.fromCharCode(event.keyCode);
+	//event.preventDefault();
+	let keys = Object.keys(sites);
+	if (keys.includes(pressed_key)) {
+		//  Lookup the corresponding key value in the dictionary
+		let target_site = sites[pressed_key];
+		//  Change windows location to the corresponding site
+		window.location.href = target_site;
+	}
+});
+
+getSites();
